e4a99921c57a7c52e7180d739908e129d2bed076
Tal Gold <talpogorelis@gmail.com>
2021-02-09 11:33:33 +0200

    added basic route + cors middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { accessControlAllowOrigin } from "./middleware/cors";
+import { router } from "./routes";
 
-const app = express();
+const app: express.Application = express();
 
-app.listen(3001, ()=> {
+app.use(router);
+app.use(accessControlAllowOrigin);
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, ()=> {
     console.log("started at: 3001");
-})
+});
