4b677cc666b3510e9db855fcb6a1a94ef6282808
Tal Gold <talpogorelis@gmail.com>
2021-02-09 13:01:03 +0200

    added basic routing to client

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './App.css';
-import { Header } from '../header/Header';
+import React from "react";
+import "./App.css";
+import { Header } from "../header/Header";
+import { Route, Switch } from "react-router-dom";
+import Signup from "../../pages/signup/Signup";
+import Login from "../../pages/login/Login";
+import Cpanel from "../../pages/cpanel/Cpanel";
 
-
-export const App:React.FC = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Switch>
+        <Route path="/register">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Cpanel />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
---
+++ client/src/pages/cpanel/Cpanel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cpanel: React.FC = () => {
+  return <div className="cpanel">Home</div>;
+};
+
+export default Cpanel;
