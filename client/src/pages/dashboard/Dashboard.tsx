4bb6dc9fbd5c1b9cc37cbdf4083d12753da8dd7f
Tal Gold <talpogorelis@gmail.com>
2021-02-10 14:49:13 +0200

    Improved Dashboard Nav + Folders design

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Img, Spacer, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import TextFieldInput from "../../common/components/TextFieldInput";
 import Menu from "./components/Menu";
@@ -10,7 +10,7 @@ const Dashboard: React.FC = () => {
     <div className="Dashboard">
       <Flex
         marginTop="5rem"
-        width="90vh"
+        width="95vh"
         height="45vh"
         background="white"
         rounded="2xl"
@@ -35,7 +35,9 @@ const Dashboard: React.FC = () => {
           <Flex justifyContent="space-between" direction="row">
             <Flex direction="column" marginRight="3rem">
               <Heading>My Cloud</Heading>
-              <Text>👋Hello User, welcome!</Text>
+              <Text marginTop="1rem" textColor="gray.400" fontWeight="medium">
+                👋 Hello User, welcome!
+              </Text>
             </Flex>
             <Flex>
               <TextFieldInput
@@ -48,7 +50,7 @@ const Dashboard: React.FC = () => {
             <Text fontWeight="bold" fontSize="3xl">
               Favorites
             </Text>
-            <Flex justifyContent="space-between">
+            <Flex justifyContent="space-between" direction="row">
               <Flex alignItems="center" direction="column">
                 <Box width="100px" height="100px" bgColor="orange.200"></Box>
                 <Text fontWeight="semibold" fontSize="2xl">
@@ -77,68 +79,106 @@ const Dashboard: React.FC = () => {
               </Text>
             </Flex>
             <Flex marginTop="1rem">
-              <Flex direction="column" marginRight="2rem">
-                <svg
-                  height="2rem"
-                  width="2rem"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-                  />
-                </svg>
-                <Text fontSize="2xl">Assets</Text>
-                <Text fontSize="xl" fontWeight="normal" textColor="gray.400">
-                  25 Files
-                </Text>
+              <Flex
+                justifyContent="space-between"
+                marginRight="2rem"
+                border="2px"
+                rounded="2xl"
+                padding="0.8rem"
+                borderColor="gray.100"
+                width="12rem"
+              >
+                <Flex direction="column">
+                  <Img src="/foldericon.png" height="3rem" width="3rem"></Img>
+                  <Text fontSize="xl">Assets</Text>
+                  <Text fontSize="lg" fontWeight="normal" textColor="gray.400">
+                    25 Files
+                  </Text>
+                </Flex>
+                <Box color="gray.300">
+                  <svg
+                    width="2rem"
+                    height="2rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </Box>
               </Flex>
-              <Flex direction="column" marginRight="2rem">
-                <svg
-                  height="2rem"
-                  width="2rem"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-                  />
-                </svg>
-                <Text fontSize="2xl">Marketing</Text>
-                <Text fontSize="xl" fontWeight="normal" textColor="gray.400">
-                  25 Files
-                </Text>
+              <Flex
+                justifyContent="space-between"
+                marginRight="2rem"
+                border="2px"
+                rounded="2xl"
+                padding="0.8rem"
+                borderColor="gray.100"
+                width="12rem"
+              >
+                <Flex direction="column">
+                  <Img src="/foldericon.png" height="3rem" width="3rem"></Img>
+                  <Text fontSize="xl">Marketing</Text>
+                  <Text fontSize="lg" fontWeight="normal" textColor="gray.400">
+                    25 Files
+                  </Text>
+                </Flex>
+                <Box color="gray.300">
+                  <svg
+                    width="2rem"
+                    height="2rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </Box>
               </Flex>
-              <Flex direction="column">
-                <svg
-                  height="2rem"
-                  width="2rem"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-                  />
-                </svg>
-                <Text fontSize="2xl">Personal</Text>
-                <Text fontSize="xl" fontWeight="normal" textColor="gray.400">
-                  25 Files
-                </Text>
+              <Flex
+                justifyContent="space-between"
+                border="2px"
+                rounded="2xl"
+                padding="0.8rem"
+                borderColor="gray.100"
+                width="12rem"
+              >
+                <Flex direction="column">
+                  <Img src="/foldericon.png" height="3rem" width="3rem"></Img>
+                  <Text fontSize="xl">Personal</Text>
+                  <Text fontSize="lg" fontWeight="normal" textColor="gray.400">
+                    25 Files
+                  </Text>
+                </Flex>
+                <Box color="gray.300">
+                  <svg
+                    width="2rem"
+                    height="2rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </Box>
               </Flex>
             </Flex>
           </Flex>
--- client/src/pages/dashboard/components/Menu.tsx
+++ client/src/pages/dashboard/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
 import React from "react";
 
 interface Props {}
@@ -19,23 +19,153 @@ const Menu: React.FC<Props> = (props: Props) => {
         >
           <div>
             <Flex direction="column">
-              <a>Cloud</a>
+              <Flex alignItems="center">
+                <Box marginRight="0.5rem">
+                  <svg
+                    height="1.5rem"
+                    width="1.5rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                    />
+                  </svg>
+                </Box>
+                <a>
+                  <Text fontWeight="semibold">Cloud</Text>
+                </a>
+              </Flex>
+              <Spacer marginTop="2rem" />
+              <Flex alignItems="center" textColor="gray.500">
+                <Box marginRight="0.5rem">
+                  <svg
+                    height="1.5rem"
+                    width="1.5rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  </svg>
+                </Box>
+                <a>
+                  <Text fontWeight="semibold">Shared</Text>
+                </a>
+              </Flex>
               <Spacer marginTop="2rem" />
 
-              <a>Shared</a>
+              <Flex alignItems="center" textColor="gray.500">
+                <Box marginRight="0.5rem">
+                  <svg
+                    height="1.5rem"
+                    width="1.5rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                  </svg>
+                </Box>
+                <a>
+                  <Text fontWeight="semibold">Favorites</Text>
+                </a>
+              </Flex>
               <Spacer marginTop="2rem" />
-
-              <a>Favorites</a>
-              <Spacer marginTop="2rem" />
-
-              <a>Trash Bin</a>
+              <Flex alignItems="center" textColor="gray.500">
+                <Box marginRight="0.5rem">
+                  <svg
+                    height="1.5rem"
+                    width="1.5rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                  </svg>
+                </Box>
+                <a>
+                  <Text fontWeight="semibold">Trash Bin</Text>
+                </a>
+              </Flex>
             </Flex>
           </div>
           <div>
             <Flex direction="column" marginTop="3rem">
-              <a>Help</a>
+              <Flex alignItems="center" textColor="gray.500">
+                <Box marginRight="0.5rem">
+                  <svg
+                    width="1.5rem"
+                    height="1.5rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </Box>
+                <a>
+                  <Text fontWeight="semibold">Help</Text>
+                </a>
+              </Flex>
               <Spacer marginTop="2rem" />
-              <a>Settings</a>
+              <Flex alignItems="center" textColor="gray.500">
+                <Box marginRight="0.5rem">
+                  <svg
+                    height="1.5rem"
+                    width="1.5rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                </Box>
+                <a>
+                  <Text fontWeight="semibold">Settings</Text>
+                </a>
+              </Flex>
             </Flex>
           </div>
         </Flex>
