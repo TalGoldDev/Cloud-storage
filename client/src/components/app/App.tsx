f5f57c47da9e4474696277a9d75c2c4567bdc168
Tal Gold <talpogorelis@gmail.com>
2021-02-11 16:54:45 +0200

    added basic bounce transition

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -7,6 +7,7 @@ import Login from "../../pages/login/Login.page";
 import { Container, Flex } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import Dashboard from "../../pages/dashboard/Dashboard";
+import { AnimatedSwitch, spring } from "react-router-transition";
 
 export const App: React.FC = () => {
   return (
@@ -25,19 +26,56 @@ export const App: React.FC = () => {
           padding="1rem"
         >
           {/* <Header /> */}
-          <Switch>
-            <Route path="/register">
-              <Signup />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/">
-              <Dashboard />
-            </Route>
-          </Switch>
+
+          <AnimatedSwitch
+            atEnter={bounceTransition.atEnter}
+            atLeave={bounceTransition.atLeave}
+            atActive={bounceTransition.atActive}
+            mapStyles={mapStyles}
+            className="route-wrapper"
+          >
+            <Route path="/register" component={Signup}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/" component={Dashboard}></Route>
+          </AnimatedSwitch>
         </Flex>
       </Flex>
     </div>
   );
+};
+
+// we need to map the `scale` prop we define below
+// to the transform style property
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
+
+// child matches will...
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8),
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1),
+  },
 };
