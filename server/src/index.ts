446d82b5ff6bf5932423ffc6b30c4c29a6a04053
Tal Gold <talpogorelis@gmail.com>
2021-02-13 21:30:39 +0200

    Installed postgres + dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { accessControlAllowOrigin } from "./middleware/cors";
 import { router } from "./routes";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: express.Application = express();
 
@@ -9,6 +12,6 @@ app.use(express.json());
 app.use(router);
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, ()=> {
-    console.log("started at: 3001");
+const server = app.listen(port, () => {
+  console.log("started at: 3001");
 });
